512fb8c333b3d4493433d47ada96c51b9f602e36
Seabass247 <sebjfk@gmail.com>
2020-02-12 09:25:45 -0500

    init

---
+++ Lab-1-BST/Lab-1-BST.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+string stringToLower(string s) {
+	string tmp = s;
+	for (int i = 0; i < s.length(); i++)
+		tmp[i] = tolower(s[i]);
+	return tmp;
+}
+
+int main()
+{
+	cout << "hello world";
+	string cmd;
+
+
+	// Loop forever.  The loop's boolean expression must always be true to indefinitely loop.
+	while (true) 
+	{
+		cin >> cmd;
+
+		string command = stringToLower(cmd);
+
+		if (command == "search")
+		{
+			string param;
+			cin >> param;
+			cout << command << " " << param << "\n";
+		}
+		else 
+		{
+			cout << command << "\n";
+		}
+	}
+}
+
